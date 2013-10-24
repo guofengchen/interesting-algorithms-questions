18cb98efa7268049a78bb151d0df709471f0bfaa
Guofeng Chen <chenguofeng08qh@gmail.com>
2013-10-24 09:36:41 +0800

    add bottom-up method for LSS

--- LSS.java
+++ LSS.java
@@ -6,6 +6,7 @@ package LSS;
  * @author chenguofeng08qh@gmail.com
  */
 public class LSS {
+    /* naive recursive */
     public static int tail(int[] a, int n) {  
         if (n ==1){return (a[0]>0 ? a[0] : 0);}
         else{
@@ -21,11 +22,29 @@ public class LSS {
             return(pre > tail ? pre: tail);
         }
     }
+    /* bottom-up*/
+   public static int bottom_up_lss(int [] a, int n){
+       if (n == 0) {return 0;}
+       else{
+           int pref =0;
+           int pretail = 0;
+           for(int j=1; j<=n; j++)
+           {
+               int tail = (pretail + a[j-1] > a[j-1] ? pretail + a[j-1]: a[j-1]);
+               int f = (pref > tail ? pref: tail);
+               pref = f;
+               pretail = tail;
+           }
+           return pref;
+       }
+
+   
+   }
    
  public static void main(String[] args) 
    { int[] a =
          { 3,-5,7, -2,4,5};
-     int result = lss(a, a.length);
+     int result = bottom_up_lss(a, a.length);
      System.out.println(result);
    } 
 }
